40de51a5592cda225557fd3f2255c86c1233a48e
apollobing <taras.career@gmail.com>
2023-09-10 11:31:44 +0200

    refactor: simplify the code - 2. Итератор для двухмерного массива int[][] [#9539]

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -14,15 +14,11 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        boolean rsl = false;
-        if (row < data.length) {
-            while (column == data[row].length && row < data.length - 1) {
-                row++;
-                column = 0;
-            }
-            rsl = data[row].length > 0;
+        while (row < data.length && column == data[row].length) {
+            row++;
+            column = 0;
         }
-        return rsl;
+        return row != data.length;
     }
 
     @Override
