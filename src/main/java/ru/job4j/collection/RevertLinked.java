6be37ad025a31a736ceeb7c1d4d01ed3318345d5
apollobing <taras.career@gmail.com>
2023-09-20 21:36:26 +0200

    6. Перевернуть связанный список [#161]

---
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -0,0 +1,76 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RevertLinked<T> implements Iterable<T> {
+    private Node<T> head;
+    private int size;
+    private int modCount;
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            size++;
+            modCount++;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+        size++;
+        modCount++;
+    }
+
+    public boolean revert() {
+        boolean rsl = false;
+        if (size > 1) {
+            Node<T> prev = null;
+            Node<T> actual = head;
+            while (actual != null) {
+                Node<T> next = actual.next;
+                actual.next = prev;
+                prev = actual;
+                actual = next;
+            }
+            head = prev;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
