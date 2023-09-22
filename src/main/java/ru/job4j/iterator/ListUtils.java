8deee6a0c47748bd6bfecfb75677e9e7964c1e0f
apollobing <taras.career@gmail.com>
2023-09-22 20:54:01 +0200

    refactor: simplify the code - 7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,20 +7,18 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        iterator.add(value);
+        list.listIterator(index).add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index + 1);
-        iterator.add(value);
+        list.listIterator(index + 1).add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> iterator = list.listIterator();
         while (iterator.hasNext()) {
-            while (iterator.hasNext() && filter.test(iterator.next())) {
+            if (filter.test(iterator.next())) {
                 iterator.remove();
             }
         }
@@ -29,21 +27,15 @@ public class ListUtils {
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> iterator = list.listIterator();
         while (iterator.hasNext()) {
-            while (iterator.hasNext() && filter.test(iterator.next())) {
+            if (filter.test(iterator.next())) {
                 iterator.set(value);
             }
         }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> iterator = list.listIterator();
         for (T el : elements) {
-            while (iterator.hasNext()) {
-                if (el == iterator.next()) {
-                    iterator.remove();
-                }
-            }
-            iterator = list.listIterator();
+            removeIf(list, i -> i == el);
         }
     }
 }
