e8e8e3e592bec6bfb21e212e3344e85dd0513e96
apollobing <taras.career@gmail.com>
2023-09-23 10:50:05 +0200

    fix: change removeAll method - 7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -34,8 +34,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        for (T el : elements) {
-            removeIf(list, i -> i == el);
-        }
+            removeIf(list, elements::contains);
     }
 }
