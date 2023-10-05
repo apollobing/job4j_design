fa96f05f469747779af2c7d2835351f4ca19e74c
apollobing <taras.career@gmail.com>
2023-10-05 19:12:15 +0200

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] arr = new int[size][size];
+        try (FileOutputStream out = new FileOutputStream("data/matrix.txt")) {
+            int count = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = (i + 1) * (j + 1);
+                    out.write((arr[i][j] + " ").getBytes());
+                    count++;
+                    if (count == size) {
+                        out.write(System.lineSeparator().getBytes());
+                        count = 0;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        multiple(10);
+        System.out.println("Done!");
+    }
+}
