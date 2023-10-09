fa727fc113c72bac990b787db8ce2fe79df457f0
apollobing <taras.career@gmail.com>
2023-10-09 19:04:59 +0200

    fix: change unavailable method - 7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,40 +1,33 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.StringJoiner;
 
 public class Analysis {
+    boolean alive = true;
+
     public void unavailable(String source, String target) {
-        StringJoiner st = new StringJoiner(System.lineSeparator());
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            final StringBuilder st = new StringBuilder();
             reader.lines()
-                    .forEach(st::add);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] strings = st.toString().split(System.lineSeparator());
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            st = new StringJoiner(System.lineSeparator());
-            String from = "";
-            String to = "";
-            for (String line : strings) {
-                if (from.isEmpty() && (line.startsWith("400") || line.startsWith("500"))) {
-                    from = line.split(" ")[1] + ";";
-                }
-                if (!from.isEmpty() && (!line.startsWith("400") && !line.startsWith("500"))) {
-                    to = line.split(" ")[1] + ";";
-                }
-                if (!from.isEmpty() && !to.isEmpty()) {
-                    st.add(from + to);
-                    from = "";
-                    to = "";
-                }
-            }
+                    .forEach(line -> {
+                        boolean available = line.startsWith("200") || line.startsWith("300");
+                        String time = line.split(" ")[1];
+                        if (alive && !available) {
+                            st.append(time).append(";");
+                            alive = false;
+                        }
+                        if (!alive && available) {
+                            st.append(time).append(";").append(System.lineSeparator());
+                            alive = true;
+                        }
+                    });
             writer.write(st.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
         analysis.unavailable("data/server.log", "data/target.csv");
