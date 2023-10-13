7b5296aec4b0a29ef8778533dcef7326b17939eb
apollobing <taras.career@gmail.com>
2023-10-13 16:18:30 +0200

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,39 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException(
+                    String.format("You use \"%d\" argument, \nbut should 2, "
+                            + "first is directory path, second is file extension", args.length)
+                    );
+        }
+        validation(args[0], args[1]);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validation(String location, String fileExtension) {
+        Path path = Paths.get(location);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Not exist \"%s\"", path.toAbsolutePath())
+            );
+        }
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory \"%s\"", path.toAbsolutePath())
+            );
+        }
+        if (!fileExtension.startsWith(".")) {
+            throw new IllegalArgumentException(
+                    String.format("Not file extension \"%s\"", fileExtension)
+            );
+        }
     }
 }
