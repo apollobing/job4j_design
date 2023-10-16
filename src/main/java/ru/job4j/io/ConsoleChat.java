6ba95f7deb290f31c32f0ea3f5a524f3a2a3544d
apollobing <taras.career@gmail.com>
2023-10-16 20:31:10 +0200

    16. Кодировка [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,80 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        List<String> phrases = readPhrases();
+        List<String> messages = new ArrayList<>();
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        boolean botActive = true;
+        System.out.println("Chat started");
+        messages.add("Chat started" + System.lineSeparator());
+        while (!userInput.equals(OUT)) {
+            System.out.print("Your message: ");
+            userInput = scanner.nextLine();
+            messages.add("Your message: " + userInput + System.lineSeparator());
+            if (userInput.equals(STOP)) {
+                botActive = false;
+                messages.add("Bot is inactive" + System.lineSeparator());
+                System.out.println("Bot is inactive");
+            }
+            if (userInput.equals(CONTINUE)) {
+                botActive = true;
+                messages.add("Bot is active" + System.lineSeparator());
+                System.out.println("Bot is active");
+                continue;
+            }
+            if (botActive && !userInput.equals(OUT)) {
+                String botAnswer = phrases.get(rand.nextInt(phrases.size()));
+                messages.add("Bot answer: " + botAnswer);
+                System.out.print("Bot answer: " + botAnswer);
+            }
+        }
+        messages.add("Chat ended");
+        saveLog(messages);
+        System.out.print("Chat ended!");
+
+    }
+
+    private List<String> readPhrases() {
+        List<String> phrases = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
+            br.lines().map(s -> s + System.lineSeparator()).forEach(phrases::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return phrases;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(pw::print);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("data/chatLog.txt", "data/botAnswers.txt");
+        cc.run();
+    }
+}
