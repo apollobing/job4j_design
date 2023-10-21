18f099c01e43741be73b6d775bf00899ea58c662
apollobing <taras.career@gmail.com>
2023-10-21 11:11:48 +0200

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,27 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        boolean learn = true;
+        String programmingLanguage = "Java";
+        char mainSymbol = 'J';
+        byte version = 17;
+        double minorMicroVersion = 0.7D;
+        short allTasks = 708;
+        long completedTasks = 394;
+        int uncompletedTasks = allTasks - (int) completedTasks;
+        float donePercentage = (float) completedTasks / allTasks * 100;
+        LOG.debug("""
+
+                        Do you learn programming? {}
+                        Which language? {}
+                        Which main symbol? {}
+                        Which version? {}
+                        Which minor and micro version? {}
+                        Total task? {}
+                        Completed? {}
+                        Uncompleted? {}
+                        Progress? {}%""",
+                learn, programmingLanguage, mainSymbol, version, minorMicroVersion,
+                allTasks, completedTasks, uncompletedTasks, donePercentage);
     }
 }
