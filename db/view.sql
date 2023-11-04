6d4a1a7420f832787de6cb26fc77d4843ef711eb
apollobing <taras.career@gmail.com>
2023-11-04 19:34:04 +0200

    1. Представления [#504792]

---
+++ db/view.sql
@@ -0,0 +1,53 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+create view show_all_data
+    as select o.id Ord,
+        o.active Status,
+        s.name Student,
+        b.name Book,
+        a.name Author
+        from books as b
+        join authors a on b.author_id = a.id
+        join orders o on o.book_id = b.id
+        join students s on s.id = o.student_id;
+
+select * from show_all_data;
