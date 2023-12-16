8e700887c287a9d4271fcbd1298523255d418c70
apollobing <taras.career@gmail.com>
2023-12-16 11:04:27 +0200

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/ood/ocp/OCPViolation.java
@@ -0,0 +1,19 @@
+package ru.job4j.ood.ocp;
+
+class Transport {
+    public void move() {
+        System.out.println("Drive? Fly? Sail?");
+    }
+}
+
+class File {
+    public void getDuration() {
+        System.out.println("Get duration of audio or video file is ok. What if we have text file?");
+    }
+}
+
+class Sportsman {
+    public void action() {
+        System.out.println("Swim? Run? Jump?");
+    }
+}
